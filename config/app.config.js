178a52fab068887fa63d481aca9fe841c85b690b
masenu <masenu95@gmail.com>
2022-11-16 00:33:07 +0300

    masenu

--- config/app.config.js
+++ config/app.config.js
@@ -1,5 +1,5 @@
 const MONGO_DB_CONFIG = {
-    DB:"mongodb://gody:1234developer@ac-hzdz42h-shard-00-00.alhowql.mongodb.net:27017,ac-hzdz42h-shard-00-01.alhowql.mongodb.net:27017,ac-hzdz42h-shard-00-02.alhowql.mongodb.net:27017/?ssl=true&replicaSet=atlas-5w7ujq-shard-0&authSource=admin&retryWrites=true&w=majority",
+    DB:"mongodb://root:Dirm2020@127.0.0.1:27017/?authSource=admin",
     PAGE_SIZE: 10
 };
 
