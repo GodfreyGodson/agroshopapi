ec822ca574b05f030e251dd1715e267ce0b08f9a
GodfreyGodson <81231519+GodfreyGodson@users.noreply.github.com>
2022-12-21 02:21:15 +0300

    Update order.model.js

--- models/order.model.js
+++ models/order.model.js
@@ -1,43 +1,68 @@
 const mongoose = require("mongoose");
 
-const order = mongoose.model(
-    "Order",
-    mongoose.Schema({
+
+const OrderSchema = new mongoose.Schema(
+    {
         userId:{
+            type:String,
+            required: true
+        },
+        cartId:{
+            type:String,
+            required:true
+        },
+
+      
+        
+        products:[
+            {
+
+               product:{
+                    type:mongoose.Schema.Types.ObjectId,
+                    ref:"Product",
+                    required: true
+                },
+                qty:{
+                    type: Number,
+                    default:1,
+                },
+    
+
+            },
+
+       
+        ],
+        phone:{
+            type:String,
+        },
+        address:{
             type:String,
             required:true,
         },
-
-        products: [
-            {
-                product:{
-                    type:mongoose.Schema.Types.ObjectId,
-                    ref:"Product",
-                    required: true,
-                },
-                amount:{
-                    type:Number,
-                    required: true,
-
-                },
-                qty:{
-                    type:Number,
-                    required:true,
-                }
-            }
-        ],
         grandTotal:{
             type:Number,
             required:true
         },
         orderStatus:{
             type:String,
-            required:true, 
+            default:"pending"
         },
+     
+          
+        
+    },{
 
-    }, {timestamps: true})
+        toJSON: {
+            transform:function(model, ret){
+                ret.productId = ret._id.toString();
+                delete ret._id;
+                delete ret.v;
+            }
+
+        }
+
+    },
+    {timestamps:true}
 );
 
-module.exports = {
-    order
-}
+module.exports = mongoose.model("Order", OrderSchema);
